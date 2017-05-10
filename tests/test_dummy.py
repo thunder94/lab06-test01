49d59b12342ef6be4253c43bef40e35aac86d999
thunder94 <jakub.marek.golec@gmail.com>
2017-05-10 18:41:23 +0200

    Tests done

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -10,11 +10,29 @@ Run with PYTHONPATH=. python tests/test_dummy.py
 class TestDummy(unittest.TestCase):
 
     def test_fun(self):
-        weekday = calculate(2001, 1, 3)
-        self.assertEqual(weekday, 2005)
+        weekday = calculate(2017, 5, 12)
+        self.assertEqual(weekday, "Friday")
+
+        weekday = calculate(2010, 6, 30)
+        self.assertEqual(weekday, "Wednesday")
+
+        weekday = calculate(2012, 2, 29)
+        self.assertEqual(weekday, "Wednesday")
+
+        weekday = calculate(2014, 2, 29)
+        self.assertEqual(weekday, None)
+
+        weekday = calculate(2100, 2, 29)
+        self.assertEqual(weekday, None)
 
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
+
+        retcode = main(("--year", "a2001", "--month", "1", "--day", "3"))
+        self.assertNotEqual(retcode, 0)
+
+        retcode = main(("--year", "-2001", "--month", "1", "--day", "3"))
+        self.assertNotEqual(retcode, 0)
 
 
 if __name__ == '__main__':
